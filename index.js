e44d4d5c82906a5cb6d7f455220d0e224c661e2e
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-07 15:58:06 +0300

    Completes codegrade

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 =  2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+const random =  Math.ceil(Math.random()*10);
+console.log(random);
+
+
+let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 8, 14, 17, 20);
+console.log(max);
